3452bdf35ad7b76964df2e4de94e383247683914
Alexis <pingrisalexis@gmail.com>
2021-11-24 16:04:23 +0100

    display recipe, ingredient list, applicances list, ustensiles list

--- src/js/build.js
+++ src/js/build.js
@@ -1,12 +1,43 @@
 import {recipes} from "../../assets/data/data";
 import {Markup} from "./markup";
 
-const recipesContainer = document.getElementById("recipes-cards-container");
-recipesContainer.innerHTML += recipes.map(recipe =>
-    new Markup(recipe).getRecipeCard()).join('');
+const getAllRecipes = () => {
+    const recipesContainer = document.getElementById("recipes-cards-container");
+    recipesContainer.innerHTML += recipes.map(elt =>
+        new Markup(elt).getRecipeCard()).join('');
+}
 
-const ingredientsList = document.getElementById("ingredients-list");
-// const allIngredients () =>
-ingredientsList.innerHTML += recipes.map(recipe =>
-    new Markup(recipe).getIngredientsList()).join('');
-// console.log(getIngredientsList())
+const getAllIngredients = () => {
+    let ingredientsList = document.getElementById("ingredients-list");
+    let allIngredients = [];
+    for (const element of recipes) {
+        element.ingredients.map(elt => allIngredients.includes(elt.ingredient) ? false : allIngredients.push(elt.ingredient))
+    }
+    ingredientsList.innerHTML = allIngredients.map(elt =>
+        new Markup(elt).getItemsList()).join('');
+}
+
+const getAllAppliances = () => {
+    let appliancesList = document.getElementById("appliances-list");
+    let allAppliances = [];
+    for (const element of recipes) {
+        allAppliances.includes(element.appliance) ? false : allAppliances.push(element.appliance)
+    }
+    appliancesList.innerHTML = allAppliances.map(elt =>
+        new Markup(elt).getItemsList()).join('');
+}
+
+const getAllUstensils = () => {
+    let ustensilsList = document.getElementById("ustensils-list");
+    let allUstensils = [];
+    for (const element of recipes) {
+        element.ustensils.map(elt => allUstensils.includes(elt) ? false : allUstensils.push(elt))
+    }
+    ustensilsList.innerHTML = allUstensils.map(elt =>
+        new Markup(elt).getItemsList()).join('');
+}
+
+getAllRecipes();
+getAllIngredients();
+getAllAppliances();
+getAllUstensils();
