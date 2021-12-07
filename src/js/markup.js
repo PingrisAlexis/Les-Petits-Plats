e4d1e79835e2dcace9d26ce5f800c1fc92c4cbf1
Alexis <pingrisalexis@gmail.com>
2021-12-07 13:03:55 +0100

    optmize bootstrap, categories list

--- src/js/markup.js
+++ src/js/markup.js
@@ -4,19 +4,17 @@ export class Markup {
     }
     getRecipeCard () {
         return `
-                <article class="recipe-card mt-3" data-filter-appliances="${this.recipe.appliance}" 
-                                                  data-filter-ingredients="${this.recipe.ingredients.map(elt => `${elt.ingredient}`)}"
-                                                  data-filter-ustensils="${this.recipe.ustensils}">          
+                <article class="recipe-card mt-3">          
                     <div class="recipe-image"></div>
                         <div class="recipe-info ">
                             <div class="card-headings container">
-                            <div class="row">
-                                <h3 class="card-title col-9">${this.recipe.name}</h3>
-                                <div class="card-time col-3 d-flex mt-1">
-                                    <i class="far fa-clock mt-1 pe-1"></i>
-                                    <span>${this.recipe.time} min</span>
+                                <div class="row">
+                                    <h3 class="card-title col-9">${this.recipe.name}</h3>
+                                    <div class="card-time col-3 d-flex mt-1">
+                                        <i class="far fa-clock mt-1 pe-1"></i>
+                                        <span>${this.recipe.time} min</span>
+                                    </div>
                                 </div>
-                            </div>
                             </div>
                          <div class="container">
                             <div class="row">
@@ -26,7 +24,7 @@ export class Markup {
                                     <strong>${elt.ingredient}</strong>: ${elt.quantity ? elt.quantity : ''}${elt.unit ? elt.unit : ''} 
                                 </li>`).join('')}
                              </ul>
-                            <div class="card-description col-sm-5">
+                            <div class="card-description col-sm-5 ">
                                 <p>
                                     ${this.recipe.description}
                                 </p>
@@ -39,18 +37,23 @@ export class Markup {
     }
     getItemsList() {
         return `
-                <li class="category-filter">
+                <li class="category-search-filter-tag">
                     <strong>${this.recipe}</strong>
                 </li>
         `
     }
-    getNoResultMessage() {
+    getSelectedTag() {
         return `
-                <div class=" mx-auto alert alert-danger w-75 align-self-center" role="alert">
-                     <span>Aucune recette ne correspond à votre critère… vous pouvez chercher « tarte aux pommes », « poisson », etc.</span>
-                </div>
-               
-               
+                <span class="p-2">${this.recipe}</span>
+                <i class="p-2 far fa-times-circle"></i>
         `
     }
+    getNoResultMessage() {
+        return `
+                <div class=" mx-auto alert alert-danger w-75 align-self-center mt-3" role="alert">
+                     <span>Aucune recette ne correspond à votre critère… vous pouvez chercher « tarte aux pommes », « poisson », etc.</span>
+                </div>                 
+        `
+    }
+
 }
