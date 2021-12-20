eda1c6fdb01a65d7df9ec9ee082e11d90d7cbb94
Alexis <pingrisalexis@gmail.com>
2021-12-20 14:37:29 +0100

    optimize algorithm 2

--- src/js/mainSearch.js
+++ src/js/mainSearch.js
@@ -1,31 +1,17 @@
 import {recipes} from "../../assets/data/data";
-import {Markup} from "./markup";
 import {searchLogic, searchRecipes} from "./searchLogic";
 
 const mainSearchFilter =  document.getElementById("main-search-filter");
-const noResultMessage = document.querySelector("#no-result-message");
 
 export const mainSearch = (value) => {
 
     let mainSearchFilteredRecipes = []
 
     for (let i=0; i< recipes.length; i++) {
-      if (recipes[i].name.toLowerCase().includes(value)) {
+      if (recipes[i].name.toLowerCase().includes(value) || recipes[i].description.toLowerCase().includes(value)) {
           mainSearchFilteredRecipes.push(recipes[i])
       }
     }
-    for (let i=0; i< recipes.length; i++) {
-        if (recipes[i].description.toLowerCase().includes(value)) {
-            mainSearchFilteredRecipes.push(recipes[i])
-        }
-    }
-
-    if (mainSearchFilteredRecipes.length === 0) {
-            noResultMessage.innerHTML = new Markup().getNoResultMessage();
-        }
-        else {
-            noResultMessage.innerHTML = "";
-        }
         return(mainSearchFilteredRecipes)
 };
 
